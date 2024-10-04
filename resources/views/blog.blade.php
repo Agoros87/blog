15cc024a7a4527b967cbfa6f851a5925750962ef
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-04 17:11:27 +0200

    Paso 6 mitad

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,8 @@
 <x-layout meta-title="Blog" meta-description="Descripcion de la página del Blog">
     <h1>Blog</h1>
+    @foreach($posts as $post)
+        <h2>{{ $post["title"] }}</h2>
+    @endforeach
+
 
 </x-layout>
