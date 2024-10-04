3bff3a2ef8dc8af03979c8cf5040ae9099c3e808
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-04 16:51:56 +0200

    Fin paso 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Inicio</title>
-
-</head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-@include('partials.navigation')
-<h1>Inicio</h1>
-</body>
-</html>
+<x-layout meta-title="Inicio" meta-description="Descripcion de la página de inicio">
+    <h1>Inicio</h1>
+    <x-slot name="sidebar">
+        <p>Home $Sidebar</p>
+    </x-slot>
+</x-layout>
