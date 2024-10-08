9b65ee5e9f71b26037ab88912700c8934a35924e
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-08 17:12:52 +0200

    Paso 7, sin el dd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class PostController extends Controller
 {
     public function index()
     {
         $posts = DB::table("posts")->get();
-
-        dd($posts);
 
 
         return view('blog', compact('posts'));
