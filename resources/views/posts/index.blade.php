46a333a957f70bab26afd062a58676a3b487fcf9
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-17 15:49:22 +0200

    Paso 20 Cambiar mensaje de bievenida de about nosotros etc centrandolo y dandole color

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,16 @@
 <x-layout meta-title="Blog" meta-description="Descripcion de la página del Blog">
 
-    <h1>Blog</h1>
+        <div class="mx-auto nt-4 max-w-6xl">
+            <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+                Blog
+            </h1>
+        </div>
+
     <a href="{{ route('posts.create') }}">{{ __('Create a new Post') }}</a>
     @foreach($posts as $post)
         <div style="display: flex;align-items: baseline">
             <h2><a href="{{ route('posts.show', $post)}}"> {{ $post->title }} </a></h2>
-            <a href="{{route('posts.edit', $post)}}">Edit</a>
+                 <a href="{{route('posts.edit', $post)}}">Edit</a>
             <form action="{{route('posts.destroy', $post)}}" method="post">
             @csrf
             @method('DELETE')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 <x-layout meta-title="Inicio" meta-description="Descripcion de la página de inicio">
-    <h1>Inicio</h1>
-    <x-slot name="sidebar">
-        <p>Home $Sidebar</p>
-    </x-slot>
+    <x-layout meta-title="Nosotros" meta-description="Descripcion de la página de Nosotros">
+
+        <div class="mx-auto nt-4 max-w-6xl">
+            <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+                Welcome
+            </h1>
+        </div>
 </x-layout>
