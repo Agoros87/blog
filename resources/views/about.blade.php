89b4a5574fcd53a784553d07775b4041262104d6
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-17 16:59:24 +0200

    Paso 21 añadimos funciones de autentificacion para asegurarnos que si no estas logeado no puedas editar ni crear posts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,8 @@
 
     <div class="mx-auto nt-4 max-w-6xl">
         <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
-            Nosotros
+            About us
         </h1>
+
     </div>
 </x-layout>
