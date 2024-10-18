245cec842686c49271f2deb0e36cc9710e7f1a92
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-18 17:46:55 +0200

    Paso 23 Retocar las vistas y arreglar el proyecto

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,18 +1,26 @@
-<x-layout :meta-title="$post->title" :meta-description="$post->body">
+<x-app-layout :meta-title="$post->title" :meta-description="$post->body">
 
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Edit Post
+        </h2>
+    </x-slot>
 
-    <h1>Edit Post</h1>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="POST" action="{{ route('posts.update', $post) }}">
+                        @include('posts.form-fields')
 
-    <a href="{{route('posts.index')}}">{{__('Back')}}</a>
+                        <x-primary-button type="submit" class="mt-4">{{ __('Send') }}</x-primary-button>
 
-    <form method="post" action="{{route('posts.update',$post)}}">
-        @csrf
-        @method('PATCH')
+                        @csrf
+                        @method('PATCH')
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        @include('posts.form-fields')
-        <br/>
-        <button type="submit">{{__('Send')}}</button>
-        <br/>
-
-    </form>
-</x-layout>
+</x-app-layout>
