297af0ee77f7b69517531a7774e62b57e46d3700
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-22 16:05:26 +0200

    Ultimop paso del proyecto, proyecto finalizado se han rehubicado varios archivos y renombrado ciertas cosas

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-<x-layout meta-title="Nosotros" meta-description="Descripcion de la página de Nosotros">
+<x-blog-layout meta-title="Nosotros" meta-description="Descripcion de la página de Nosotros">
 
     <div class="mx-auto nt-4 max-w-6xl">
         <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
@@ -6,4 +6,4 @@
         </h1>
 
     </div>
-</x-layout>
+</x-blog-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
-<x-layout meta-title="Inicio" meta-description="Descripcion de la página de inicio">
+<x-blog-layout meta-title="Inicio" meta-description="Descripcion de la página de inicio">
 
         <div class="mx-auto nt-4 max-w-6xl">
             <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
                 Welcome
             </h1>
         </div>
-</x-layout>
+</x-blog-layout>
